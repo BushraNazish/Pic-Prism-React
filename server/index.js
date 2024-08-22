f52bc40e06e6cc67a16c9b6d043c4542fc82e3f2
BushraNazish <bushranazish1998@gmail.com>
2024-08-22 18:30:46 +0530

    MongoDB connected

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@
 
 // Express should be called in this file
 const express = require("express");
-
 const dotenv = require("dotenv");
+const { readdirSync } = require("fs");
+const { connectDb } = require("./connection");
+
+// import the route here
+// const authRoute = require("./routes/authRoutes");
 
 // Binding this env
 dotenv.config();
@@ -15,10 +19,26 @@ const app = express();
 // Define a port [ Port is like a DOOR ]
 const port = process.env.PORT || 5000;
 
+connectDb()
 // Making routes
 app.get("/", (req, res) => {
     res.send("<center><h1>Server Running...</h1></center>")
 })
+
+// How to use routes
+// app.use("/api", authRoute);
+
+// importing and using routes dynamically
+readdirSync("./routes").map((route) => 
+    app.use("/api", require(`./routes/${route}`))
+);
+// console.log(readdirSync("./routes"))
+
+// types of requests :
+// 1. GET -> To get the data from the server
+// 2. POST -> To post the data to the server
+// 3. PUT -> To update the data on the server
+// 4. DELETE -> To delete the data from the server
 
 // Listen to the Server 
 app.listen(port,() => {
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,13 @@
+// This is the normal method
+// const express = require("express")
+// const router = express.Router()
+
+const { login, signup } = require("../controllers/authController");
+
+// Ye hai mentos zindagi
+const router = require("express").Router();
+
+router.post("/login", login);
+router.post("/signup", signup);
+
+module.exports = router;

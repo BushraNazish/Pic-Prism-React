4b250d2f7805a86c6c7abb8c025619b9a24b501f
BushraNazish <bushranazish1998@gmail.com>
2024-08-24 16:04:37 +0530

    Setting up Redux toolkit in login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,41 @@
-import { Link } from "react-router-dom"
+import { useDebugValue, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { login } from "../../store/slices/authSlice";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(import.meta.env.VITE_API_URL + "/login", {
+        email,
+        password,
+      });
+      const data = await res.data;
+      toast.success(data.message);
+
+      // dispatch karna hai login -> jo bhi data aa raha hai sab push karna hai state me
+      dispatch(login(data));
+      navigate(`/${data.role}/profile`);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="mt-20 sm:mt-10 min-h-screen flex items-center justify-center w-full">
+    <div className="mt-20 sm:mt-10 min-h-screen flex items-center justify-center w-full" >
     <div className="bg-white shadow-md rounded-3xl px-5 py-6 w-full sm:w-[27vw]">
       <h1 className="text-2xl font-bold text-center mb-4">Let's Connect!</h1>
-      <form>
+      <form onSubmit={handleLogin}>
 
         {/* For email */}
         <div className="mb-4">
@@ -20,6 +50,8 @@ const Login = () => {
             name="email" 
             id="email"
             placeholder="your@email.com" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
           />
         </div>
@@ -37,6 +69,8 @@ const Login = () => {
             name="password" 
             id="password"
             placeholder="Enter your password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
           />
         </div>
@@ -59,4 +93,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;

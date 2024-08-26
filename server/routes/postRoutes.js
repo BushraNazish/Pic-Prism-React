bcf6513a3aee41480f9e6936adf1b98a128ada60
BushraNazish <bushranazish1998@gmail.com>
2024-08-26 22:32:20 +0530

    Added all Post related Routes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -3,11 +3,21 @@ const { verifyToken } = require("../middlewares/verifyToken");
 const { 
     createPost, 
     getAllPosts, 
-    getMyPosts 
+    getMyPosts, 
+    deletePost,
+    searchPosts,
+    addToFavourites,
+    removeFromFavourites,
+    getFavourites
 } = require("../controllers/postController");
 
 router.post("/post/create", verifyToken, createPost);
 router.get("/post/getAll", getAllPosts);
 router.get("/post/myPosts", verifyToken, getMyPosts);
+router.delete("/post/delete/:id", verifyToken, deletePost);
+router.get("/posts/search", searchPosts);
+router.put("/post/addToFavourites/:postId", verifyToken, addToFavourites);
+router.put("/post/removeFromFavourites/:postId", verifyToken, removeFromFavourites);
+router.get("/posts/favourites", verifyToken, getFavourites);
 
 module.exports = router;
